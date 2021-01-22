1b17b9348310cda595319e0f60478bf942fbdaae
saiyaswanth.kudumala@gmail.com <github.open123>
2021-01-22 01:39:19 -0500

    Added Validations for the project objects

--- src/main/java/com/fullstack/ppmtool/domain/Project.java
+++ src/main/java/com/fullstack/ppmtool/domain/Project.java
@@ -3,18 +3,31 @@ package com.fullstack.ppmtool.domain;
 import java.util.Date;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private long id;
+	@NotBlank(message="Project Name is required")
 	private String projectName;
+	@NotBlank(message="Project Identifier is required")
+	@Size(min=5, max=5, message="Please use 4 to 5 characters allowed")
+	@Column(updatable= false, unique=true)
 	private String projectIdentifier;
+	@NotBlank(message="Project Description is required")
 	private String description;
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date start_date;
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date end_date;
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date created_At;
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date updated_At;
 	public Project() {
 		System.out.println("Project Constructor Creation");
