e2891d873f3952fe00e563e8057a1495e58145f6
saiyaswanth.kudumala@gmail.com <github.open123>
2021-02-18 08:14:39 -0500

    Solving git error

--- ppmtool-react-client/src/action/projectAction.js
+++ ppmtool-react-client/src/action/projectAction.js
@@ -4,6 +4,7 @@ import { GET_ERRORS } from "./types";
 export const createProject = (project, history) => async dispatch => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
+    console.log(history);
     history.push("/dashboard");
   } catch (err) {
     dispatch({
